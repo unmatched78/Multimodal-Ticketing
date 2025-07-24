98b8f9879d9800f00372837a4a5b69e885d145c6
unmatched78 <marvinavi24@gmail.com>
2025-07-24 22:18:36 +0200

    base: metadata & sessions

--- db/base.py
+++ db/base.py
@@ -1,1 +1,7 @@
 #declarative base, metadata
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+#metadata for the database
+from sqlalchemy import MetaData
+metadata = MetaData()
