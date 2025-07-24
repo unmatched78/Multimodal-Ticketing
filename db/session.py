c8d377952d61468b2ca67aa70543f2e2ec2b898c
unmatched78 <marvinavi24@gmail.com>
2025-07-24 22:19:13 +0200

    # Base class for declarative models

--- db/session.py
+++ db/session.py
@@ -1,1 +1,8 @@
 #SQLAlchemy engine & SessionLocal
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from core.config import settings
+
+engine = create_engine(settings.DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base class for declarative models
